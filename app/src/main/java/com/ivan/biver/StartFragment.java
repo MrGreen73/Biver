3ea63e799c3cba9646d7355bed273c46957ab7ce
Ivan <damag666@gmail.com>
2018-03-05 02:20:04 +0300

    Подключен Firebase Сделан дизайн стартового фрагмента

--- app/src/main/java/com/ivan/biver/StartFragment.java
+++ app/src/main/java/com/ivan/biver/StartFragment.java
@@ -3,27 +3,40 @@ package com.ivan.biver;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class StartFragment extends Fragment {
+
+    Button mSignWithGoogle;
+    Button mSignWithTwitter;
+    Button mSignWithFacebook;
+    Button mSignWithEmail;
+    Button mSignWithPhone;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v =  inflater.inflate(R.layout.fragment_start, container, false);
-/*
-        Snackbar
 
-        Snackbar
-                .make(parent, "Покормил кота?", Snackbar.LENGTH_LONG)
-                .setAction("Да", snackOnClickListener)
-                .setActionTextColor(Color.MAGENTA) // цвет текста у кнопки действия
-                .show();*/
+        mSignWithGoogle = v.findViewById(R.id.btnInGoogle);
 
+        mSignWithGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Snackbar
+                        .make(v, "Хорошая работа", Snackbar.LENGTH_LONG)
+//                        .setAction("Спасибо", null)
+                        .show();
+
+            }
+        });
 
         return v;
 
