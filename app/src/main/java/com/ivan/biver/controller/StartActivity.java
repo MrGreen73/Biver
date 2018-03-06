8fa84e4018ea1e8dc7481ece2bfb6370c726581c
Ivan <damag666@gmail.com>
2018-03-06 19:22:48 +0300

    Добавлен Toolbar

--- app/src/main/java/com/ivan/biver/controller/StartActivity.java
+++ app/src/main/java/com/ivan/biver/controller/StartActivity.java
@@ -4,16 +4,23 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.ivan.biver.R;
 import com.ivan.biver.view.StartFragment;
 
 public class StartActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        toolbar = findViewById(R.id.mainToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Вход");
 
 
         //Переход на стартовый фрагмент
