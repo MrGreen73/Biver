d3512bc9bf91bfb0555b98fc68e65486cdf00341
Ivan <damag666@gmail.com>
2018-03-07 12:37:47 +0300

    Добавлены фрагменты для меню

--- app/src/main/java/com/ivan/biver/controller/MainActivity.java
+++ app/src/main/java/com/ivan/biver/controller/MainActivity.java
@@ -3,16 +3,27 @@ package com.ivan.biver.controller;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ivan.biver.R;
+import com.ivan.biver.view.DiscountFragment;
+import com.ivan.biver.view.OrderFragment;
+import com.ivan.biver.view.StartFragment;
+import com.ivan.biver.view.StoreFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private FragmentManager fragmentManager;
+    private Fragment container;
+    private Fragment fragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,14 +31,38 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_store:
+                    fragmentManager = getSupportFragmentManager();
+                    container = fragmentManager.findFragmentById(R.id.mainContainer);
+                    fragment = new StoreFragment();
+
+                    if (container == null) {
+
+                        fragmentManager.beginTransaction().replace(R.id.mainContainer, fragment).commit();
+
+                    }
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_discount:
+                    fragmentManager = getSupportFragmentManager();
+                    container = fragmentManager.findFragmentById(R.id.mainContainer);
+                    fragment = new DiscountFragment();
+
+                    if (container == null) {
+
+                        fragmentManager.beginTransaction().replace(R.id.mainContainer, fragment).commit();
+
+                    }
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_order:
+                    fragmentManager = getSupportFragmentManager();
+                    container = fragmentManager.findFragmentById(R.id.mainContainer);
+                    fragment = new OrderFragment();
+
+                    if (container == null) {
+
+                        fragmentManager.beginTransaction().replace(R.id.mainContainer, fragment).commit();
+
+                    }
                     return true;
             }
             return false;
@@ -42,8 +77,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment container = fragmentManager.findFragmentById(R.id.mainContainer);
+        Fragment fragment = new StoreFragment();
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        if (container == null) {
+
+            fragmentManager.beginTransaction().add(R.id.mainContainer, fragment).commit();
+
+        }
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
---
+++ app/src/main/java/com/ivan/biver/view/DiscountFragment.java
@@ -0,0 +1,24 @@
+package com.ivan.biver.view;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ivan.biver.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DiscountFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_discount, container, false);
+    }
+
+}
